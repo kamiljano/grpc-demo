8cc14a2a08c2c515aba212a65dfc4c9c7d6ba6b9
Kamil Janowski <kamil.janowski@smartly.io>
2025-03-05 11:19:53 +0200

    hello from server actions

--- apps/client/src/app/actions.tsx
+++ apps/client/src/app/actions.tsx
@@ -1,5 +1,14 @@
 'use server';
 
+import { echo } from '@/app/message-service';
+import { EchoRequest } from '@/grpc/service';
+
 export const getData = async (): Promise<string> => {
-  return process.env.PATH || 'nothing';
+  const result = await echo(
+    new EchoRequest({
+      message: 'Hello from the server actions',
+    }),
+  );
+
+  return result?.message || 'No message returned';
 };
